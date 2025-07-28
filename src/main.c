9f2788957833d03a21b002fee8ec2b801945f5eb
Eduardo Lucca <edlucca@student.hive.fi>
2025-07-28 20:34:37 +0300

    envp working

--- src/main.c
+++ src/main.c
@@ -36,8 +36,75 @@
 // 	return (0);
 // }
 
+char	**ft_get_env_paths(char **envp)
+{
+	char	*path;
+	char	*command;
+	int		i;
+
+	i = 0;
+	command = "PATH";
+	while (envp[++i])
+	{
+		if(ft_strncmp(command, envp[i], ft_strlen(command)) == 0
+				&& envp[i][ft_strlen(command)] == '=')
+			path = ft_strchr(envp[i], '=') + 1;
+		if (!path)
+			return (NULL);
+	}
+	return (ft_split(path, ':'));
+}
+
+char	*ft_find_path(char *cmd, char **envp)
+{
+	int		i;
+	char	*tmp;
+	char	**paths;
+	char	*full_path;
+
+	paths = ft_get_env_paths(envp);
+	if (!paths)
+		return (NULL); // might need review
+	i = -1;
+	while (paths[++i])
+	{
+		tmp = ft_strjoin(paths[i], "/");
+		if (!tmp)
+			return (ft_free_array(paths), NULL);
+		full_path = ft_strjoin(tmp, cmd);
+		free(tmp);
+		if (!full_path)
+		{
+			return (ft_free_array(paths), NULL);
+		}
+		if (access(full_path, F_OK)== 0)
+		{
+			return (ft_free_array(paths), full_path);
+		}
+		ft_printf("full_path: %s\n", full_path); // testing
+		free(full_path);
+	}
+	return (ft_free_array(paths), NULL);
+}
+
 void	ft_parse_cmds(t_pipex *pipex, int argc, char **argv, char **envp)
 {
+	int		i;
+	char	**cmd;
+
+	pipex->argv= ft_calloc((argc - 2 - pipex->here_doc), sizeof(char *));
+	if (!pipex->argv)
+		return ;	
+	i = 1 + pipex->here_doc;
+	while (++i < argc -1)
+	{
+		cmd = ft_split(argv[i], ' ');
+		if (!cmd)
+		{
+		}
+		pipex->argv[i - 2 - pipex->here_doc] = ft_find_path(cmd[0], envp);
+		ft_free_array(cmd);
+	}
 }
 
 int	get_infile(t_pipex *pipex, char **argv)
